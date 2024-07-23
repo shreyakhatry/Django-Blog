05f2892420a6b91dbc1ab46004b491d67886a7d9
Shreya Khatry <97456783+shreyakhatry@users.noreply.github.com>
2024-07-23 17:21:21 +0530

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,22 +1,10 @@
-# from django.contrib import admin
-# from .models import Post
-
-# # Register your models here.
-
-# admin.site.register(Post)
 from django.contrib import admin
 from .models import Post
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'date_posted')
-    list_filter = ('date_posted', 'author')
-    search_fields = ('title', 'content')
+# Register your models here.
 
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:  # If this is a new post
-            obj.author = request.user
-        super().save_model(request, obj, form, change)
+admin.site.register(Post)
+
 
 
 
